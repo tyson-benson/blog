02276927477b80e2e2da773edef08aee5c88d491
Tyson Benson <me@tyson.net.au>
2021-10-10 21:30:24 +1100

    Add favicon

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -127,13 +127,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `Tyson Benson - Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-196.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
